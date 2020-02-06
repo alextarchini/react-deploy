8bd7a2ea5d094118cb41b1617bf1c5ee41c66d53
Aleksandra Tarchini <alex.tarchini@gmail.com>
2020-02-06 11:45:17 +0100

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-deploy/precache-manifest.b4f12a7fb8f6bf681bda6631b26faac1.js"
+  "/react-deploy/precache-manifest.57e1ff8d15866b4b342ae6f49e8eb1b4.js"
 );
 
 self.addEventListener('message', (event) => {
