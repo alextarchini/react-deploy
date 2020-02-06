2394a3024857607d529a345ca0ecc4978968d830
Aleksandra Tarchini <alex.tarchini@gmail.com>
2020-02-06 10:56:32 +0100

    Updates

---
+++ precache-manifest.b4f12a7fb8f6bf681bda6631b26faac1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcad3c54916211e3392b076752b09541",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "595d1f5265f8ee23de44",
+    "url": "/react-deploy/static/css/main.bf92f223.chunk.css"
+  },
+  {
+    "revision": "e3b2df6bf43c8abe2fd3",
+    "url": "/react-deploy/static/js/2.7f38fdf4.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-deploy/static/js/2.7f38fdf4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "595d1f5265f8ee23de44",
+    "url": "/react-deploy/static/js/main.3164a487.chunk.js"
+  },
+  {
+    "revision": "2a49018d5d1f167a6be3",
+    "url": "/react-deploy/static/js/runtime-main.8751e28a.js"
+  },
+  {
+    "revision": "11995fc9d3f0a088e21b4fb15a8affcd",
+    "url": "/react-deploy/static/media/AlexPic.11995fc9.jpg"
+  }
+]);
